61e5ee07e4aacf4cf773ed6f8dbd3d2afff4e573
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2026-09-17 15:54:30 +0100

    adding transaction list and ui feature

--- apps/api/src/payment/providers/paystack.client.ts
+++ apps/api/src/payment/providers/paystack.client.ts
@@ -7,20 +7,11 @@ export class PaystackClient {
   private readonly client: AxiosInstance;
 
   constructor(private readonly configService: ConfigService) {
-    console.log({
-      paystackKeyExists: !!this.configService.get<string>(
-        'PAYSTACK_SECRET_KEY',
-      ),
-      paystackKeyPrefix: this.configService
-        .get<string>('PAYSTACK_SECRET_KEY')
-        ?.slice(0, 8),
-    });
-
     this.client = axios.create({
       baseURL: 'https://api.paystack.co',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer $${this.configService.getOrThrow<string>('PAYSTACK_SECRET_KEY')}`,
+        Authorization: `Bearer ${this.configService.getOrThrow<string>('PAYSTACK_SECRET_KEY')}`,
       },
     });
   }
